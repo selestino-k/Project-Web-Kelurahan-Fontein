ee2cc1c98beff5659c8aa0044608aee645670aaa
selestino-k <sandikroon23@gmail.com>
2022-08-20 15:21:23 +0800

    made the navbar sticky + minor changes

--- src/main.js
+++ src/main.js
@@ -1,16 +1,9 @@
- /**Sticky navigation */
-
- let navbar = $(".navbar");
-
- $(window).scroll(function(){
-    console.log(window.innerHeight);
-    console.log($(".section").offset().top);
- });
 
  
  /**Count animation */
 
- let nCount = function(selector){
+ 
+ /*let nCount = function(selector){
     $(selector).each(function(){
         $(this).animate({
             Counter: $(this).text()
@@ -32,3 +25,62 @@
         nCount(".rectangle > h1");
     }
  })
+ */
+
+ /*let counts=setInterval(updated);
+        let upto=0;
+        function updated(){
+            var count= document.getElementById(".rectangle");
+            count.innerHTML=++upto;
+            if(upto===4688)
+            {
+                clearInterval(counts);
+            }
+        }
+*/
+
+let filterarray = [];
+
+let docarray = [
+    {
+        id:1,
+        name : "sk kelurahan",
+        src : "./assets/sk fontein cover.jpg",
+        desc : "Dokumen publikasi yang bisa diunduh masyarakat "
+    },
+    {
+        id:2,
+        name : "tata cara",
+        src : "./assets/sk fontein cover.jpg",
+        desc : "Dokumen publikasi yang bisa diunduh masyarakat "
+    },
+    {
+        id:3,
+        name : "rencana anggaran",
+        src : "./assets/sk fontein cover.jpg",
+        desc : "Dokumen publikasi yang bisa diunduh masyarakat "
+    }
+]
+
+showdoc(docarray);
+
+function showdoc (currarray){
+    for(var i = 0; i<currarray.length; i++){
+        document.getElementById("card").innerHTML += `
+        <div class = "col-md-4 mt-3">
+        <div class="card p-3 ps-5 pe-5">
+            <h6 class="text-capitalize text-center">${currarray[i].name}</h6>
+            <img src="${currarray[i].src}" width="100%" height="400px"/>
+            <p class="mt-2">${currarray[i].desc}</p>
+            <button class="btn btn-primary w-100 mx-auto">Unduh</button>
+        </div>
+        </div>
+        `
+        
+
+    }
+}
+
+
+
+ 
