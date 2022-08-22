05cf9d7cf0b0d8d53e429e48e5a8fe7e3faeac44
selestino-k <sandikroon23@gmail.com>
2022-08-22 14:40:52 +0800

    added website darkmode toggle + minor changes

--- src/main.js
+++ src/main.js
@@ -39,47 +39,47 @@
         }
 */
 
-let filterarray = [];
+// let filterarray = [];
 
-let docarray = [
-    {
-        id:1,
-        name : "sk kelurahan",
-        src : "./assets/sk fontein cover.jpg",
-        desc : "Dokumen publikasi yang bisa diunduh masyarakat "
-    },
-    {
-        id:2,
-        name : "tata cara",
-        src : "./assets/sk fontein cover.jpg",
-        desc : "Dokumen publikasi yang bisa diunduh masyarakat "
-    },
-    {
-        id:3,
-        name : "rencana anggaran",
-        src : "./assets/sk fontein cover.jpg",
-        desc : "Dokumen publikasi yang bisa diunduh masyarakat "
-    }
-]
+// let docarray = [
+//     {
+//         id:1,
+//         name : "sk kelurahan",
+//         src : "./assets/sk fontein cover.jpg",
+//         desc : "Dokumen publikasi yang bisa diunduh masyarakat "
+//     },
+//     {
+//         id:2,
+//         name : "tata cara",
+//         src : "./assets/sk fontein cover.jpg",
+//         desc : "Dokumen publikasi yang bisa diunduh masyarakat "
+//     },
+//     {
+//         id:3,
+//         name : "rencana anggaran",
+//         src : "./assets/sk fontein cover.jpg",
+//         desc : "Dokumen publikasi yang bisa diunduh masyarakat "
+//     }
+// ]
 
-showdoc(docarray);
+// showdoc(docarray);
 
-function showdoc (currarray){
-    for(var i = 0; i<currarray.length; i++){
-        document.getElementById("card").innerHTML += `
-        <div class = "col-md-4 mt-3">
-        <div class="card p-3 ps-5 pe-5">
-            <h6 class="text-capitalize text-center">${currarray[i].name}</h6>
-            <img src="${currarray[i].src}" width="100%" height="400px"/>
-            <p class="mt-2">${currarray[i].desc}</p>
-            <button class="btn btn-primary w-100 mx-auto">Unduh</button>
-        </div>
-        </div>
-        `
+// function showdoc (currarray){
+//     for(var i = 0; i<currarray.length; i++){
+//         document.getElementById("card").innerHTML += `
+//         <div class = "col-md-4 mt-3">
+//         <div class="card p-3 ps-5 pe-5">
+//             <h6 class="text-capitalize text-center">${currarray[i].name}</h6>
+//             <img src="${currarray[i].src}" width="100%" height="400px"/>
+//             <p class="mt-2">${currarray[i].desc}</p>
+//             <button class="btn btn-primary w-100 mx-auto">Unduh</button>
+//         </div>
+//         </div>
+//         `
         
 
-    }
-}
+//     }
+// }
 
 
 
